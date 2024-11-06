4e46ab746371f18dbd84c1a4bce0aab932c87ed8
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-11-06 11:57:15 -0300

    fix: resolve a logica do programa para iniciar um contador de tempo para o descanso

--- lua/pomodorow/init.lua
+++ lua/pomodorow/init.lua
@@ -1,5 +1,6 @@
 local utils = require("pomodorow.utils")
 local commands = require("pomodorow.user_commands")
+local kind = require("pomodorow.kind")
 
 commands.setup()
 
@@ -7,24 +8,27 @@ local M = {}
 
 local timer = nil
 
+M.status = ''
+
 M.in_break = false
 
+M.visible_remaining_time = true
+
+M.work_time = 25 * 60
+M.break_time = 5 * 60
 M.remaining_time = M.work_time
 
-M.visible_remaining_time = false
-
 function M.setup(work_time, break_time)
-  M.work_time = (work_time * 60) or (25 * 60)
-  M.break_time = (break_time * 60) or (5 * 60)
+  M.work_time = (work_time and work_time * 60) or M.work_time
+  M.break_time = (break_time and break_time * 60) or M.break_time
+  M.remaining_time = M.work_time
 end
 
 function M.start_timer()
   if timer then
-    vim.notify("Timer is running", vim.log.levels.INFO)
+    vim.notify("O timer está em execução" .. kind.icons.execution, vim.log.levels.INFO)
     return
   end
-
-  M.remaining_time = M.in_break and M.break_time or M.work_time
 
   timer = vim.loop.new_timer()
 
@@ -32,20 +36,25 @@ function M.start_timer()
     M.remaining_time = M.remaining_time - 1
     local mins, secs = utils.get_minutes_and_seconds(M.remaining_time)
 
-    if M.visible_remaining_time then
-      vim.notify("Remaining time: " .. mins .. ":" .. secs, vim.log.levels.INFO)
+    if M.in_break then
+      M.status = '(break)'
+    else
+      M.status = '(work)'
     end
 
-    if timer and M.remaining_time <= 0 then
-      timer:stop()
-      timer:close()
-      timer = nil
+    if M.visible_remaining_time then
+      vim.notify("Tempo restante" .. M.status .. ":" .. mins .. ":" .. secs, vim.log.levels.INFO)
+    end
 
-      M.in_break = not M.in_break
+    if M.remaining_time <= 0 then
       if M.in_break then
-        vim.notify("Break time!", vim.log.levels.INFO)
+        vim.notify("Tempo de trabalho iniciado!" .. kind.icons.screen, vim.log.levels.INFO)
+        M.in_break = false
+        M.remaining_time = M.work_time
       else
-        vim.notify("Work time!", vim.log.levels.INFO)
+        vim.notify("Tempo de descanso iniciado!" .. kind.icons.start, vim.log.levels.INFO)
+        M.in_break = true
+        M.remaining_time = M.break_time
       end
     end
   end))
@@ -56,18 +65,20 @@ function M.stop_timer()
     timer:stop()
     timer:close()
     timer = nil
-    vim.notify("Timer stopped", vim.log.levels.INFO)
+    M.remaining_time = M.work_time
+    M.in_break = false
+    vim.notify("Timer parado" .. kind.icons.stop, vim.log.levels.INFO)
   else
-    vim.notify("Timer is not running", vim.log.levels.INFO)
+    vim.notify("O timer não está em execução" .. kind.icons.pause, vim.log.levels.INFO)
   end
 end
 
 function M.show_timer()
   if timer then
     local mins, secs = utils.get_minutes_and_seconds(M.remaining_time)
-    vim.notify("Remaining time: " .. mins .. ":" .. secs, vim.log.levels.INFO)
+    vim.notify("Tempo restante: " .. kind.icons.clock .. mins .. ":" .. secs, vim.log.levels.INFO)
   else
-    vim.notify("No timer is running", vim.log.levels.INFO)
+    vim.notify("Nenhum timer está em execução" .. kind.icons.execution, vim.log.levels.INFO)
   end
 end
 
@@ -77,13 +88,18 @@ end
 
 function M.set_work_and_break()
   M.stop_timer()
-  vim.ui.input({ prompt = "Enter work time (in minutes): " }, function(input)
-    M.work_time = tonumber(input) * 60
+  vim.ui.input({ prompt = "Insira o tempo de trabalho (em minutos): " }, function(work_input)
+    if work_input then
+      M.work_time = tonumber(work_input) * 60
+    end
+    vim.ui.input({ prompt = "Insira o tempo de descanso (em minutos): " }, function(break_input)
+      if break_input then
+        M.break_time = tonumber(break_input) * 60
+      end
+      M.remaining_time = M.work_time
+      M.start_timer()
+    end)
   end)
-  vim.ui.input({ prompt = "Enter break time (in minutes): " }, function(input)
-    M.break_time = tonumber(input) * 60
-  end)
-  M.start_timer()
 end
 
 return M
